02275449146cb44293c9f4fe123a899a12e74aaf
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-04-18 23:34:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
